0ced06aa47003f84db877a9e5a5851e276f13dec
Pawel Prochot <pawel.prochot@yougov.com>
2019-11-10 14:13:25 +0100

    Quick adding popular Polish stores.

---
+++ test/e2e/euro-com-uris-test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const should = require('should');
+const testHelper = require('./test-helper');
+
+const priceFinder = testHelper.priceFinder;
+const verifyPrice = testHelper.verifyPrice;
+const verifyItemDetails = testHelper.verifyItemDetails;
+
+describe('price-finder for EuroCom Store URIs', () => {
+  describe('testing [ENTER ITEM NAME HERE] item', () => {
+    // TODO provide URI and contents for test here!
+    const uri = '';
+
+    it('should respond with a price for findItemPrice()', (done) => {
+      priceFinder.findItemPrice(uri, (err, price) => {
+        should(err).be.null();
+        verifyPrice(price);
+        done();
+      });
+    });
+
+    it('should respond with a price, and the right category and name for findItemDetails()', (done) => {
+      priceFinder.findItemDetails(uri, (err, itemDetails) => {
+        should(err).be.null();
+        verifyItemDetails(itemDetails, '', '');
+        done();
+      });
+    });
+  });
+});
---
+++ test/unit/sites/komputronik-test.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const should = require('should');
+const cheerio = require('cheerio');
+const siteUtils = require('../../../lib/site-utils');
+const KomputronikSite = require('../../../lib/sites/komputronik');
+
+const VALID_URI = 'http://komputronik.pl/123/product';
+const INVALID_URI = 'http://www.bad.com/123/product';
+
+describe('The Komputronik Site', () => {
+  it('should exist', () => {
+    should.exist(KomputronikSite);
+  });
+
+  describe('isSite() function', () => {
+    it('should return true for a correct site', () => {
+      should(KomputronikSite.isSite(VALID_URI)).be.true();
+    });
+
+    it('should return false for a bad site', () => {
+      should(KomputronikSite.isSite(INVALID_URI)).be.false();
+    });
+  });
+
+  it('should throw an exception trying to create a new site with an incorrect uri', () => {
+    should.throws(() => {
+      /* eslint-disable no-new */
+      new KomputronikSite(INVALID_URI);
+      /* eslint-enable no-new */
+    });
+  });
+
+  describe('a new Komputronik Site', () => {
+    let site;
+
+    beforeEach(() => {
+      site = new KomputronikSite(VALID_URI);
+    });
+
+    it('should exist', () => {
+      should.exist(site);
+    });
+
+    it('should return the same URI for getURIForPageData()', () => {
+      should(site.getURIForPageData()).equal(VALID_URI);
+    });
+
+    it('should return false for isJSON()', () => {
+      should(site.isJSON()).be.false();
+    });
+
+    describe('with a populated page', () => {
+      let $;
+      let bad$;
+      let price;
+      let category;
+      let name;
+
+      beforeEach(() => {
+        price = 1.99;
+        category = siteUtils.categories.OTHER;
+        name = 'Some Product';
+
+        // TODO provide content for unit test here!
+        $ = cheerio.load('');
+        bad$ = cheerio.load('<h1>Nothin here</h1>');
+      });
+
+      it('should return the price when displayed on the page', () => {
+        const priceFound = site.findPriceOnPage($);
+        should(priceFound).equal(price);
+      });
+
+      it('should return -1 when the price is not found', () => {
+        const priceFound = site.findPriceOnPage(bad$);
+        should(priceFound).equal(-1);
+      });
+
+      it('should always return the category', () => {
+        const categoryFound = site.findCategoryOnPage($);
+        should(categoryFound).equal(category);
+      });
+
+      it('should return the name when displayed on the page', () => {
+        const nameFound = site.findNameOnPage($, category);
+        should(nameFound).equal(name);
+      });
+
+      it('should return null when the name is not displayed on the page', () => {
+        const nameFound = site.findNameOnPage(bad$, category);
+        should(nameFound).be.null();
+      });
+    });
+  });
+});
